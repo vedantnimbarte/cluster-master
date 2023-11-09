7211395f097be9fa8baefb645b4ff3d90438aab4
Vedant Nimbarte <nimbartevedant@gmail.com>
2023-11-09 23:02:52 +0530

    Added cluster manager methods

---
+++ src/ClusterManager.ts
@@ -0,0 +1,49 @@
+import cluster, { Worker } from 'cluster';
+import os from 'os';
+
+interface ClusterOptions {
+  maxClusters: number;
+}
+
+class ClusterManager {
+  private static instance: ClusterManager;
+  private maxClusters: number;
+
+  private constructor(options: ClusterOptions) {
+    this.maxClusters = options.maxClusters;
+    this.initCluster();
+  }
+
+  public static getInstance(options: ClusterOptions): ClusterManager {
+    if (!this.instance) {
+      this.instance = new ClusterManager(options);
+    }
+    return this.instance;
+  }
+
+  private initCluster() {
+    cluster.setupPrimary({
+      exec: __filename,
+    });
+
+    for (let i = 0; i < this.maxClusters; i++) {
+      this.spawnCluster();
+    }
+  }
+
+  private spawnCluster() {
+    if (Object.keys(cluster.workers).length < this.maxClusters) {
+      const worker: Worker = cluster.fork();
+      console.log(`Cluster ${worker.process.pid} started`);
+    }
+  }
+
+  public sendMessageToCluster(clusterId: number, message: string) {
+    const worker = cluster.workers[clusterId];
+    if (worker) {
+      worker.send(message);
+    }
+  }
+}
+
+export default ClusterManager;
